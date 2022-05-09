ca7e36c137a90fed8c7fbc872236fd548159a876
Gabriel Fontán <60316747+BobbyESP@users.noreply.github.com>
2022-05-09 10:03:36 +0200

    Switched to english language in just one line lol

--- app/src/main/java/com/devaloteam/dictionary/feature_dictionary/data/remote/DictionaryAPI.kt
+++ app/src/main/java/com/devaloteam/dictionary/feature_dictionary/data/remote/DictionaryAPI.kt
@@ -6,8 +6,8 @@ import retrofit2.http.Path
 
 interface  DictionaryAPI {
 
-    //API URL to get the JSON Response with dictionary data. "es" string can be changed for the language that you want (see https://dictionaryapi.dev to know if the language you want is available)
-    @GET("/api/v2/entries/es/{word}")
+    //API URL to get the JSON Response with dictionary data. "en" string can be changed for the language that you want (see https://dictionaryapi.dev to know if the language you want is available)
+    @GET("/api/v2/entries/en/{word}")
     suspend fun getWordInfo(
         @Path("word") word: String
     ): List<WordInfoDto>
